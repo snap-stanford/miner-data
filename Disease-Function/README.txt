231f951238addd10cc2f03d26a08bc78bb3d3229
Viswajith Venugopal <viswajithiii@gmail.com>
2016-05-26 14:48:58 -0700

    Added Disease-Function.

---
+++ Disease-Function/README.txt
@@ -0,0 +1,26 @@
+Current datasets containing disease-function information:
+- CTD
+
+Workflow for creating crossnet tables for disease-disease relationships:
+
+Pre-requisites:
+Must have the disease modes table from CTD (MESH), and the function mode table from
+GO.
+/path/to/disease_mode/miner-disease-1-CTD_MESH-20160521.tsv
+/path/to/function_mode/miner-function-0-GO-20160521.tsv
+
+Input files/directories:
+/path/to/input/CTD_dir  (from CTD)
+
+Intermediate files:
+/path/to/intermediate/ctd_disease_func_parsed.tsv
+
+Output files:
+/path/to/output/miner-disease-function-20160521.tsv
+/path/to/output/miner-disease-function-0-CTD-20160521.tsv
+
+# Create intermediate files
+python make_disease_func_ctd.tsv /path/to/input/CTD_dir --output_dir /path/to/intermediate/
+
+# Create cross net files
+python ../Utils/create_snap_crossnet_table.py /path/to/intermediate/ctd_disease_func_parsed.tsv /path/to/disease_mode/miner-disease-1-CTD_MESH-20160521.tsv /path/to/function_mode/miner-function-0-GO-20160521.tsv CTD 0 --output_dir /path/to/output/ --skip_missing_ids
---
+++ Disease-Function/make_disease_func_ctd.py
@@ -0,0 +1,61 @@
+'''
+file : make_disease_func_ctd.py
+author: Viswajith Venugopal
+
+Goes over the disease function file in CTD and creates a table with the disease id
+and GO id of the function.
+
+Usage:
+python make_disease_func_ctd <input_dir> [--output_dir OUTPUT_DIR]
+
+Positional Arguments:
+input_dir   : The directory with all the CTD TSVs
+
+Optional Arugments: 
+--output_dir : Directory to create output files. Defaults to the current working directory.
+
+Example Usage:
+Input Dir: CTD/0416_CTD
+
+Output directory : outputs/disease_func/
+
+Comamnd line:
+python make_disease_func_ctd.py CTD/0416_CTD --output_dir outputs/disease_func/
+
+Output: 
+ctd_disease_func_parsed.tsv
+'''
+
+from collections import defaultdict
+import os
+import argparse
+
+def load_disease_functions_ctd(ctd_dir):
+    f1 = open(os.path.join(ctd_dir, 'CTD_Disease-GO_biological_process_associations.tsv'), 'r')
+    f2 = open(os.path.join(ctd_dir, 'CTD_Disease-GO_cellular_component_associations.tsv'), 'r')
+    f3 = open(os.path.join(ctd_dir, 'CTD_Disease-GO_molecular_function_associations.tsv'), 'r')
+    global_list = []
+    for f in [f1, f2, f3]:
+        for line in f:
+            if line.startswith('#'):
+                continue
+            sp_line = line.strip('\n').split('\t')
+            disease_id = 'MESH:' + sp_line[1]
+            go_id = sp_line[3]
+            global_list.append((disease_id, go_id))
+    return global_list
+
+parser = argparse.ArgumentParser(description='Parse CTD to find disease-function links.')
+parser.add_argument('input_dir', help='Input files directory. This should be the directory with all the CTD TSVs.')
+parser.add_argument('--output_dir', help='Directory to output files', default='.')
+args = parser.parse_args()
+
+output_fname = os.path.join(args.output_dir, "ctd_disease_func_parsed.tsv")
+
+disease_func_list = load_disease_functions_ctd(args.input_dir)
+
+with open(output_fname, 'w') as out_f:
+    out_f.write('#Disease Function links from CTD.\n')
+    for (disease_id, go_id) in disease_func_list:
+        out_f.write('\t'.join([disease_id, go_id]))
+        out_f.write('\n')
