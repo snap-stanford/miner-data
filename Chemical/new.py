4540759b19eaacfc900c274105368f1cb7180b68
Farzaan Kaiyom <farzaan@madmax4.stanford.edu>
2019-08-28 12:18:15 -0700

    made new scripts work

--- Chemical/new.py
+++ Chemical/new.py
@@ -17,19 +17,22 @@ args = parser.parse_args()
 
 masterTable = os.path.join(args.output_dir,"miner-chemical-" + dateStr + ".tsv")
 drugbankTable = os.path.join(args.output_dir, "miner-chemical-0-drugbank-" + dateStr + ".tsv")
-with open(args.input_file, 'r') as input, open(masterTable, 'w') as master,open(eqTable, 'w') as eqTable:
+with open(args.input_file, 'r') as input, open(masterTable, 'w') as master, open(drugbankTable,'w') as drugTable:
     master.write('# snap_id\tdataset_id\n')
-    drugbankTable.write('# snap_id\tdataset_id\tname\n')
-    eqTable.write('# Equivalence table for mode chemical\n')
-    eqTable.write('# snap_id_1\tsnap_id_2\n')
+    drugTable.write('# snap_id\tdataset_id\tname\n')
+    #eqTable.write('# Equivalence table for mode chemical\n')
+    #eqTable.write('# snap_id_1\tsnap_id_2\n')
     for line in input:
         if line.startswith('#'):
             continue
-        if line.startswith('D'):
-            line = line.strip().split(sep)
+	spline =line.strip().split(sep)
+        if line.startswith('DB') and len(spline)>1:
+            line = spline
             id = line[0]
             name = line[1]
+            if name == "":
+                name = "NULL"
             snapId = snapIdPrefix + str(idNum)
             idNum += 1
             master.write(snapId + sep + id + '\n')
-            drugbankTable.write(snapId + sep + id + name + '\n')
+            drugTable.write(snapId + sep + id + name + '\n')
